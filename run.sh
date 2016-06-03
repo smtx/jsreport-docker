277e30f16a5773686405f259d50ab0ed47de33b9
Sebastian Rios <seba.rios@gmail.com>
2016-06-03 11:59:15 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,33 +1,37 @@
 # file run.sh
 #!/bin/sh
 
-if [ -d "/jsreport" ]; then
+if [ -d "/jsreport" ]; then 
 
   # link data folder from mounted volume
 
-  if [ ! -d "/jsreport/data" ]; then
+  if [ ! -d "/jsreport/data" ]; then 
     mkdir "/jsreport/data"
   fi
 
   ln -nsf "/jsreport/data" "/home/jsreport/data"
 
-  # copy default config
+  # copy default config  
 
-  if [ ! -f "/jsreport/prod.config.json" ]; then
-    cp "/home/jsreport/prod.config.json" "/jsreport/prod.config.json"
+  if [ ! -f "/jsreport/prod.config.json" ]; then    
+    cp "/home/jsreport/prod.config.json" "/jsreport/prod.config.json"    
   fi
 
-  if [ ! -f "/jsreport/node_modules/jsreport/prod.config.json" ]; then
-    cp "/home/jsreport/prod.config.json" "/jsreport/node_modules/jsreport/prod.config.json"
+  if [ ! -f "/jsreport/dev.config.json" ]; then
+      cp "/home/jsreport/dev.config.json" "/jsreport/dev.config.json"
   fi
 
   # delete default config and link from volume
-
+	
   rm -f "/home/jsreport/prod.config.json"
   ln -s "/jsreport/prod.config.json" "/home/jsreport/prod.config.json"
+
+  rm -f "/home/jsreport/dev.config.json"
+  ln -s "/jsreport/dev.config.json" "/home/jsreport/dev.config.json"
 
   chown -R jsreport:jsreport /jsreport
 fi
 
 su jsreport
-sudo node "/home/jsreport/server.js" --httpsPort=2945
+
+sudo NODE_ENV=${NODE_ENV:-production} node "/home/jsreport/server.js" --httpsPort=2945
